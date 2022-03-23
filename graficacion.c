43bda2c79b2fcae92f130d219753164b6a50be54
luisgalicia93 <luisgalicia93@aragon.unam.mx>
2022-03-22 20:37:13 -0600

    comentario2

--- graficacion.c
+++ graficacion.c
@@ -1,2 +1,14 @@
 //programa de graficacion 
 //comentario de prueba 
+//segundo comentario de prueba
+/* Les envío la rúbrica de evaluación del primer parcial 
+
+
+a) Elementos que debe cumplir el programa
+1.  Se debe definir como parámetro  de entrada el número de puntos a graficar en el polígono, ejemplo 3,4,5,6
+2. Definir como se tomaran las puntos a graficar si están en orden o en desorden
+3. En caso de estar en orden,  la figura se cerrara con el punto A - N, donde N es el valor de la última coordenada
+4. En caso de estar desordenados, definir un criterio para ordenar y dibujar  el polígono
+5. Preguntar el tipo de transformación que se va a realizar, si es una a la vez o varias. En ambos casos aplicar lo que corresponda, si son varias deben de cumplirse las propiedades de las transformaciones
+6. Dar como resultado final la aplicación de la transformación y/o transformaciones
+*/
