b750d2a44ed8ed3664822c10f6d048f8227ffe14
luisgalicia93 <luisgalicia93@aragon.unam.mx>
2022-03-26 15:08:59 -0600

    traslacion y escalamiento con adiccion

--- ProyectoRotar.cpp
+++ ProyectoRotar.cpp
@@ -17,7 +17,10 @@ int main(){
 	char orden=' ';
 	char opcionpuntopivote=' ';
 	int radianes = 0 ;
-	int angulo = 0;
+	float angulo = 0.0;
+	float desplazamientox=0.0;
+	float desplazamientoy=0.0;
+	float escalamiento=1.0;
 	cout<<"Programa de Transformaciones geometricas \n "; 
 	cout<<"En este programa puede rotar, escalar y trasladar";
 	cout<<"\nDigite el numero de puntos que desea graficar: "; cin>>filas;
@@ -64,22 +67,8 @@ cout<<"va a hacer en orden el ingreso de los punntos de ser asi ingrese (s) si n
 	}
       numPunto =-1;
 
-	
-	
-    
-int opcion = 0;
-do{
-	cout<<"\n\n--------------Opciones de tranformadas-----------------------\n";
-	cout<<"   1. Rotar \n";
-	cout<<"   2. Escalar \n";
-	cout<<"   3. Trasladar \n";
-	cout<<"   4. Salir \n ";
-	cout<<"\nQue tranformada desea realizar (1-4)\n";
-	cin>>opcion;
-	
-	switch(opcion){
-		case 1: cout<<"------------Rotamiento--------------";
-		
+
+
 			cout<<"\nEl punto pivote es uno de los puntos del poligono? de ser asi ingrese (s) si no (n)"<<endl;
     cin>>opcionpuntopivote;
     
@@ -110,16 +99,94 @@ do{
 			cout<<"Digite un numero ["<<filas<<"]["<<j<<"]: ";
 			cin>>puntos_poligono[filas][j];
 		}
-        puntos_poligono[filas][0] = punto_pivote[0][0];
-    	puntos_poligono[filas][1] = punto_pivote[0][1];
+        punto_pivote[0][0]=puntos_poligono[filas][0] ;
+    	punto_pivote[0][1]=puntos_poligono[filas][1]; 
 		cout<<"("<<punto_pivote[0][0]<<",";
         cout<<punto_pivote[0][1]<<")";
     }
 		
+	
+	
+    /*se tienen que acumular las trasformaciones en variables globales antes de trasformase para que la tras
+	formacion correspondiente se realice solo una vez, por el principio de addicion */
+int opcion = 0;
+do{
+	cout<<"\n\n--------------Opciones de tranformadas-----------------------\n";
+	cout<<"   1. Rotar \n";
+	cout<<"   2. Escalar \n";
+	cout<<"   3. Trasladar \n";
+	cout<<"   4. Salir \n ";
+	cout<<"\nQue tranformada desea realizar (1-4)\n";
+	cin>>opcion;
+	
+	switch(opcion){//revisar opcion de rotamiento 
+		case 1: cout<<"------------Rotamiento--------------";
+		/**/
+		float angulotemp=0.0;
 	cout<<"\nDigite los grados que rotaran los puntos:";
-	cin>>angulo;
-	if (angulo == 360 ){
-		cout<< "Los puntos quedaron de la misma forma en que los agregó";
+	cin>>angulotemp;
+	angulo=angulo+angulotemp;//para acumular los angulos de rotamiento
+	
+	
+		break;
+		
+		case 2: cout<<"------------Escalamiento--------------"<<endl;
+				float es;
+				es=1.0;
+				cout<<"ingrese el factor de escala"<<endl;
+				cin>>es;
+				cout<<"factor de escalamiento:"<<es<<endl;
+				//para el escalamiento acumulado
+				escalamiento=escalamiento*es;
+		break;
+		
+		case 3: cout<<"------------Traslacion--------------";
+		float x,y;
+		cout<<"ingrese desplazamiento en x"<<endl;
+		cin>>x;
+		cout<<"ingrese desplazamiento en y"<<endl;
+		cin>>y;
+		//para el desplazamiento acumulado 
+		desplazamientox=desplazamientox+x;
+		desplazamientoy=desplazamientoy+y;
+	
+	
+		break;
+		default: cout<<"Esa opcion no estï¿½ en el menu, vuelva a elegir";
+	
+	}
+	
+} while(opcion != 4);
+
+//aqui se hacen los desplazamientos acumulados en una sola vez 
+for(int i=0;i<filas;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				if(j%2==0)
+				{puntos_poligono[i][j]=puntos_poligono[i][j]+desplazamientox;}
+				else
+				{puntos_poligono[i][j]=puntos_poligono[i][j]+desplazamientoy;}
+			}
+		}
+//aqui se hacen los escalamientos acumulados en una sola vez
+
+for(int i=0;i<filas;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				float temp=0.0;
+				temp=((puntos_poligono[i][j]-punto_pivote[0][j])*escalamiento);
+				puntos_poligono[i][j]=temp+punto_pivote[0][j];
+				
+				
+			}
+		}
+
+//aqui se hacen las rotaciones acumuladas de una sola vez
+/*
+if (angulo == 360 ){
+		cout<< "Los puntos quedaron de la misma forma en que los agregï¿½";
 		
 	} if (angulo >360) {
 		cout<<"Ese angulo no existe, digite un angulo valido";
@@ -149,21 +216,21 @@ do{
 			cout<<"("; cout<<rotar[i][j]<<")";
 		}
 		cout<<"\n";
-	} 
-		break;
-		
-		case 2: cout<<"------------Escalamiento--------------";
-		break;
-		
-		case 3: cout<<"------------Traslacion--------------";
-		break;
-		default: cout<<"Esa opcion no está en el menu, vuelva a elegir";
-	
-	}
-	
-} while(opcion != 4);
+*/
 
-	
+
+//aqui se imprimen los puntos despues de todas las trasformaciones 
+
+		cout<<"los puntos despues de las trasformaciones"<<endl;
+				for(int i=0;i<filas;i++){
+	        //numPunto++;
+			//cout<<numPunto;
+			
+		for(int j=0;j<columnas;j++){
+			
+			cout<<"("; cout<<puntos_poligono[i][j]<<")";
+		}cout<<endl;}
+		
 	getch();
 	return 0;
 }
